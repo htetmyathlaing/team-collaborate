57de4add08120caf0d833e5935a96a96ca26ab06
Htet Myat Hlaing <htetmyathlaing.htd@gmail.com>
2018-08-24 02:06:34 +0630

    24 Aug 2018 Test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
     <!-- Styles -->
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="{{ mix('/css/app.css') }}"> --}}
     
     <style type="text/css" media="screen">
         a:hover{
